689c6643f4c31ac9d3dfec8c274a4ecd42e85cde
kartikeymalvi <kartikeymalviya02@gmail.com>
2025-01-14 14:27:19 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,61 @@
+# operators==>
+# 1.arithmatic ==> (+,-,*,/,% ,||(floor),**(power)) (this operator returns value)
+
+
+
+# 2.comparision==> (it returns boolean value(true ,false))
+#  ( ==,!=,>,<,>=,<=)
+# x=10
+# y=20
+# print(x==y)
+# print(x!=y)
+# print(x>y)
+# print(x<y)
+# print(x>=y)
+# print(x<=y)
+# ( list type in python)
+# x=[10]
+# y=[10]
+# print(x==y)
+# print(x!=y)
+# print(x>y)
+# print(x<y)
+# print(x>=y)
+# print(x<=y)
+# x=10
+# y=10
+# print(x==y)
+# print(x!=y)
+# print(x>y)
+# print(x<y)
+# print(x>=y)
+# print(x<=y)
+
+# 3.Assignment(it returns a value)
+# ( =,+=,-=,*=,/=,%=,//=,**=) 
+# x=10
+#  x+=1(increment by 1) output=11
+# 4.Logical operator 
+# and,or,not
+# and ==>( in this all condition will be true then it returns true)
+# x=10
+# y=20
+# z=30
+# print(x<y and y<z) 
+# print(x<y and y>z)
+
+# or ( in this any one condition will be true then it returns true)
+# x=10
+# y=20
+# z=30
+# print(x<y or y<z) 
+# print(x<y or y>z)
+
+
+
+# not (it inverts the results )
+# x=10
+# y=20
+# z=30
+# print(not (x<y or y<z)) 
+# print(not(x<y or y>z))
