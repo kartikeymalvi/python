aac9068864b0d61c7896229c046d411d6aad6956
kartikeymalvi <kartikeymalviya02@gmail.com>
2025-01-15 14:28:24 +0530

    updated operators

--- operators.py
+++ operators.py
@@ -59,3 +59,45 @@
 # z=30
 # print(not (x<y or y<z)) 
 # print(not(x<y or y>z))
+
+#4.Membership ==>
+# str1='kartikey '
+# print('n' in str1)
+# print('n' not in str1)
+# str2='Kartikey'
+# print('K' in str2)
+# print('K' not in str2)
+
+
+
+#Identity==>
+# x=10
+# y=20
+# print(x is y)
+# y=10
+# z=10
+# print(y is z)
+a=[10]
+b=[10]
+print(a is b) # (is compares address of an output)
+print(a==b)  # (is compares value of an output)
+
+
+#Bitwise operator ==>(&,!,~,^,<<,>>)
+
+
+#Number stystem==>(collection of digit)
+# Binary    =>(0,1)   ( 2 digit used)        inbuild function to convert bin()
+# octal     =>(0-7)    ( 8 digit used)      inbuild function to convert oct()
+# decimal   =>(0-9)      ( 10 digit used)    by deafult
+# hexadecimal => (0-9)(A-F)(a-f)   ( 16 digit used)  inbuild function to convert hex()
+# c=10
+# print(bin(c))
+# print(oct(c))
+# print(hex(c))
+
+
+
+
+
+
