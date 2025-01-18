ea4c120f0938e65618d5cf43d0e2221ebcfde1c0
kartikeymalvi <kartikeymalviya02@gmail.com>
2025-01-18 14:04:49 +0530

    updated python object types

---
+++ call_by_refrence.py
@@ -0,0 +1,6 @@
+#python is working on  call by refrence
+
+x=10
+y=10
+print(id(x)) #same memory  address
+print(id(y)) #same memory address
---
+++ py_object_type.py
@@ -0,0 +1,38 @@
+# python has two object types ==>
+# 1. immutable=>( int,str,tuple,frozenset) (can not be changed)
+# 2. mutable =>(list,dict,set) (can  be changed)
+# immutable===>
+# 1.integer
+x=10
+y=10
+print(id(x),id(y))
+
+# 2.=> string
+a='kartikey'
+b='kartikey'
+print(id(a),id(b))
+
+# 3. ==>Tuple 
+x1=(10,20,30,'kartikey')
+y1=(10,20,30,'kartikey')
+print(id(x1),id(y1))
+
+#Mutable==>
+
+#1.list ==>
+
+l1=[10,20,30,'kartik']
+l2=[10,20,30,'kartik']
+print(id(l1),id(l2))
+
+#2.  dictionary===>
+
+d1= {'name':'kartikey','age':25,'city':'bhopal'}
+d2= {'name':'kartikey','age':25,'city':'bhopal'}
+print(id(l1),id(l2))
+
+#3. set===>
+s1={ 10,20,30, 'kartik'}
+s2={ 10,20,30, 'kartik'}
+print(id(s1),id(s2))
+
