3cbf453b5a974b32e57196e8bcf65b374505e25d
kartikeymalvi <kartikeymalviya02@gmail.com>
2025-01-20 14:27:18 +0530

    updated indexing

--- indexing.py
+++ indexing.py
@@ -18,4 +18,29 @@
 #syntax==> indexing is used on collections 
 # collection.index('obj')
 
+#indexing in list
+str1= 'python'
+print(str1.index('p')) 
+# str2= 'python'
+# print(str2.index('p',0,1))  (throws error) 
+
+str2='kartikey'
+print(str2[0])
+print(str2[1])
+print(str2[2])
+print(str2[3])
+
+
+mylist=[10,20,30,'kartikey',50]
+# print(mylist.index(10))  (gives index number in output)
+# print(mylist.index('kartikey'))
+# print(mylist.index('kartikey',0,4))#(collection.index('objname',startpoint,endpoint))
+print(mylist[3])  # (gives value in output)
+
+
+#indexing in tuple
+mytuple=(10,20,30,'kartikey',50)
+print(mytuple.index('kartikey'))
+print(mytuple.index('kartikey',1,4))  #(collection.index('objname',startpoint,endpoint))
+print(mytuple[3])
 
---
+++ slicing.py
@@ -0,0 +1,12 @@
+# for slicing collection must is in form of ordered collection
+#   ex.==>(string,list,tuple)
+# syntax ==> collection[startpoint:stop:step/jump/direction]
+# rule==> 1. check direction of the step
+#    if step/jump/direction is empty(collection [startpoint:endpoint]) than by default
+#    its 1.
+#     +ve step (+ve diection)
+#     -ve step (-ve diection)
+
+#     2.check start-stop direction
+#     3. if both dirction are matching then we get output otherwise
+#     it gives empty output
