5b7f2cd04b23067ff587ba1ad7cf896fdffa8dd4
kartikeymalvi <kartikeymalviya02@gmail.com>
2025-01-22 14:28:28 +0530

    updated datatypes in python

---
+++ datatypes.py
@@ -0,0 +1,61 @@
+#IN-BUILT FUNCTIONS==>
+# print()
+# type()
+# input()
+# id()
+# len()
+# min()
+# max()
+# sum() (it is not used for string)
+
+ #asqui values===>
+#  (A-Z)=65-90
+#  (a-z)=97-122
+#  (0-9)=48-57
+
+#DATATYPES
+
+#1.Numeric==>( float,int,complax)
+x=20
+y=10
+z=x+y
+print(type(z))  #int
+x1=20
+y1=10
+z1=x1*y1
+print(type(z1))  #int
+a=10
+b=200
+c=a/b
+print(type(c))  #float
+a1=12.5
+b1=22.3
+c1=a1+b1
+print(type(c1))     #float
+ 
+l=10.5+3j
+m=12.5+3j
+o=l+m
+print(type(o))    #complex
+print(o)    #complex
+
+
+# 2.String      (from 2 to 8 is called collection datatype)
+s1=input("enter your name :")
+print(s1)  #string 
+print(type(s1))   #string 
+
+p='Python'
+print(max(p)) #return max asqui value
+ 
+r='kartikey'
+print(len(r)) #return length
+
+
+
+# 3.List
+# 4.tuple
+# 5.Dictionary (in this key is always be unique,it is also called as maped Data type)
+# 6.Set
+# 8.frozensetSet  (it does not hold duplicate contents,only holds unique values)
+# 9.Boolean
