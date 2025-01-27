d593d54ed64469fc4da9cf283f9fd00d164d6312
kartikeymalvi <kartikeymalviya02@gmail.com>
2025-01-27 13:55:31 +0530

    updated dictionery in python

---
+++ dictionery.py
@@ -0,0 +1,18 @@
+# Dict===>
+# indexing in dictionary is not supported
+# slicing in dictionary is not supported
+# key must be unique
+# value can be duplicate
+# key is always in string format
+# multable in nature
+# represented in {} with comma (,)
+# syntax==>
+# d1=={key:value,key1:value1,key2:value2}
+
+#IN BUILT FUNCTIONS FOR Dictionary=>
+d1={'age':25,'city':'bhopal','name':'kartikey'}  #(in this min amd max is always mesured by alphabtical order)
+print(max(d1))
+print(min(d1))
+print(len(d1))
+print(type(d1))
+print(id(d1))
